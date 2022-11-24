9072435fb30dee718a016a201e32f4f596575714
Ismael Martín Moakil <85709107+MiM14@users.noreply.github.com>
2022-11-24 19:18:12 +0100

    Observer added not finished yet

--- app/src/main/java/com/moaimar/movie_2022/presentation/MovieDetailViewModel.kt
+++ app/src/main/java/com/moaimar/movie_2022/presentation/MovieDetailViewModel.kt
@@ -1,7 +1,13 @@
 package com.moaimar.movie_2022.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.moaimar.movie_2022.domain.Movie
 
 class MovieDetailViewModel(): ViewModel() {
+    val movieDetailPublisher: MutableLiveData<Movie> by lazy{
+        MutableLiveData<Movie>()
+    }
+
 
 }
--- app/src/main/java/com/moaimar/movie_2022/presentation/MovieFeedViewModel.kt
+++ app/src/main/java/com/moaimar/movie_2022/presentation/MovieFeedViewModel.kt
@@ -1,5 +1,20 @@
 package com.moaimar.movie_2022.presentation
 
-class MovieFeedViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.moaimar.movie_2022.domain.GetMoviesFeedUseCase
+import com.moaimar.movie_2022.domain.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+class MovieFeedViewModel(private val getMoviesFeedUseCase: GetMoviesFeedUseCase) : ViewModel() {
+    private val movieFeedPublisher: MutableLiveData<Movie> by lazy {
+        MutableLiveData<Movie>()
+    }
+    fun loadMovieFeed(){
+        viewModelScope.launch(Dispatchers.IO){
+            movieFeedPublisher.postValue(getMoviesFeedUseCase.execute())
+        }
+    }
 }
